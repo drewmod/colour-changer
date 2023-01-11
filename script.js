e253272f675de717e703df690eb29ba2536c3f10
drewmod <bade65799@gmail.com>
2023-01-11 12:32:16 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let btn = document.getElementById("button")
+
+
+
+
+
+let RandColor=()=> {
+    //16 digit hex decimal number
+    let val = "0123456789ABCDEF";
+    // const pound sign
+    let cons ="#"
+    let cond ="wepted"
+    // to get the six digit we loop our val together with pound 
+    
+    for(let i=0; i<6; i++){
+        cons = cons + val[Math.floor(Math.random()*16)]
+ 
+    }
+
+    return cons
+}
+
+console.log(RandColor());
+
+function ChangeColor() {
+  document.body.style.backgroundColor = RandColor()  
+}
+
+btn.addEventListener("click", ChangeColor)
